51eda629e2247ad311026b0605becafe8b37991d
Rémy Le Calloch <remy@lecalloch.net>
2022-11-15 10:18:27 +0100

    Change: rework plugin import wizard

--- activity_browser/ui/wizards/plugin_import_wizard.py
+++ activity_browser/ui/wizards/plugin_import_wizard.py
@@ -1,36 +1,40 @@
 # -*- coding: utf-8 -*-
-from pathlib import Path
-import py7zr
 import importlib
 import sys
+import os
+import shutil
+import tempfile
+from pathlib import Path
+from distutils.dir_util import copy_tree
 
 import brightway2 as bw
+import py7zr
 from PySide2 import QtWidgets, QtCore
 from PySide2.QtCore import Signal, Slot
 
 from ...signals import signals
 from ..style import style_group_box
-from ...settings import project_settings
 
 
 class PluginImportWizard(QtWidgets.QWizard):
     LOCATE = 1
-    CONFIRM = 2
-    IMPORT = 3
+    IMPORT = 2
+    CONFIRM = 3
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Plugin Import Wizard")
         self.setWindowModality(QtCore.Qt.ApplicationModal)
+        self.plugin = None
 
         # Construct and bind pages.
         self.locate_page = LocatePage(self)
-        self.confirmation_page = ConfirmationPage(self)
         self.import_page = ImportPage(self)
+        self.confirmation_page = ConfirmationPage(self)
 
         self.setPage(self.LOCATE, self.locate_page)
-        self.setPage(self.CONFIRM, self.confirmation_page)
         self.setPage(self.IMPORT, self.import_page)
+        self.setPage(self.CONFIRM, self.confirmation_page)
         self.setStartId(self.LOCATE)
 
         # with this line, finish behaves like cancel and the wizard can be reused
@@ -39,31 +43,20 @@ class PluginImportWizard(QtWidgets.QWizard):
 
         # thread management
         self.button(QtWidgets.QWizard.CancelButton).clicked.connect(self.cancel_thread)
-        self.button(QtWidgets.QWizard.CancelButton).clicked.connect(self.cancel_extraction)
 
-        import_signals.connection_problem.connect(self.show_info)
         import_signals.import_failure.connect(self.show_info)
-        import_signals.import_failure_detailed.connect(self.show_detailed)
 
     def closeEvent(self, event):
         """ Close event now behaves similarly to cancel, because of self.reject.
-
         This allows the import wizard to be reused, ie starts from the beginning
         """
         self.cancel_thread()
-        self.cancel_extraction()
         event.accept()
 
     def cancel_thread(self):
         print('\nPlugin import interrupted!')
         import_signals.cancel_sentinel = True
         self.cleanup()
-
-    def cancel_extraction(self):
-        process = getattr(self.extractor, "extraction_process", None)
-        if process is not None:
-            process.kill()
-            process.communicate()
 
     def cleanup(self):
         if self.import_page.main_worker_thread.isRunning():
@@ -76,17 +69,6 @@ class PluginImportWizard(QtWidgets.QWizard):
         title, message = info
         QtWidgets.QMessageBox.information(self, title, message)
 
-    @Slot(object, tuple, name="showDetailedMessage")
-    def show_detailed(self, icon: QtWidgets.QMessageBox.Icon, data: tuple) -> None:
-        title, message, *other = data
-        msg = QtWidgets.QMessageBox(
-            icon, title, message, QtWidgets.QMessageBox.Ok, self
-        )
-        if other:
-            other = other[0] if len(other) == 1 else other
-            msg.setDetailedText("\n\n".join(str(e) for e in other))
-        msg.exec_()
-
 
 class LocatePage(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
@@ -94,8 +76,6 @@ class LocatePage(QtWidgets.QWizardPage):
         self.wizard: QtWidgets.QWizard = parent
         self.path = QtWidgets.QLineEdit()
         self.registerField("plugin_path*", self.path)
-        self.name = QtWidgets.QLineEdit()
-        self.registerField("plugin_name*", self.name)
         self.path.setReadOnly(True)
         self.path.textChanged.connect(self.changed)
         self.path_btn = QtWidgets.QPushButton("Browse")
@@ -121,11 +101,7 @@ class LocatePage(QtWidgets.QWizardPage):
 
     def nextId(self):
         self.setField("plugin_path", self.path.text())
-        # set plugin_name
-        filename = Path(self.field("plugin_path")).name
-        plugin_name = '.'.join(filename.split('.')[:-1])
-        self.setField("plugin_name", plugin_name)
-        return PluginImportWizard.CONFIRM
+        return PluginImportWizard.IMPORT
 
     @Slot(name="browseFile")
     def browse(self) -> None:
@@ -152,53 +128,9 @@ class LocatePage(QtWidgets.QWizardPage):
         return self.complete
 
 
-class ConfirmationPage(QtWidgets.QWizardPage):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.wizard = parent
-        self.setCommitPage(True)
-        self.setButtonText(QtWidgets.QWizard.CommitButton, 'Import Plugin')
-        self.current_project_label = QtWidgets.QLabel('empty')
-        self.plugin_name_label = QtWidgets.QLabel('empty')
-        self.path_label = QtWidgets.QLabel('empty')
-
-        layout = QtWidgets.QVBoxLayout()
-        box = QtWidgets.QGroupBox("Import Summary:")
-        box_layout = QtWidgets.QVBoxLayout()
-        box_layout.addWidget(self.current_project_label)
-        box_layout.addWidget(self.plugin_name_label)
-        box_layout.addWidget(self.path_label)
-        box.setLayout(box_layout)
-        box.setStyleSheet(style_group_box.border_title)
-        layout.addWidget(box)
-        self.setLayout(layout)
-
-    def initializePage(self):
-        self.current_project_label.setText(
-            'Current Project: <b>{}</b>'.format(bw.projects.current))
-        self.plugin_name_label.setText(
-            'Name of the plugin: <b>{}</b>'.format(
-                self.field('plugin_name')))
-        self.path_label.setText(
-            'Path to .plugin file:<br><b>{}</b>'.format(
-                self.field('plugin_path')))
-
-    def validatePage(self):
-        """
-        while a worker thread is running, it's not possible to proceed to the import page.
-        this is required because there is only one sentinel value for canceled imports
-        """
-        running = self.wizard.import_page.main_worker_thread.isRunning()
-        return not running
-
-    def nextId(self):
-        return PluginImportWizard.IMPORT
-
-
 class ImportPage(QtWidgets.QWizardPage):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setFinalPage(True)
         self.wizard = parent
         self.complete = False
         self.relink_data = {}
@@ -207,14 +139,14 @@ class ImportPage(QtWidgets.QWizardPage):
 
         self.unarchive_label = QtWidgets.QLabel('Decompressing the 7z archive:')
         self.unarchive_progressbar = QtWidgets.QProgressBar()
-        self.finalizing_label = QtWidgets.QLabel('Finalizing:')
-        self.finalizing_progressbar = QtWidgets.QProgressBar()
+        self.loading_label = QtWidgets.QLabel('Loading plugin content:')
+        self.loading_progressbar = QtWidgets.QProgressBar()
         self.finished_label = QtWidgets.QLabel('')
 
         layout.addWidget(self.unarchive_label)
         layout.addWidget(self.unarchive_progressbar)
-        layout.addWidget(self.finalizing_label)
-        layout.addWidget(self.finalizing_progressbar)
+        layout.addWidget(self.loading_label)
+        layout.addWidget(self.loading_progressbar)
         layout.addStretch(1)
         layout.addWidget(self.finished_label)
         layout.addStretch(1)
@@ -224,14 +156,15 @@ class ImportPage(QtWidgets.QWizardPage):
         # progress signals
         import_signals.unarchive_finished.connect(self.update_unarchive)
         import_signals.unarchive_failed.connect(self.report_failed_unarchive)
-        import_signals.finalizing.connect(self.update_finalizing)
-        import_signals.finished.connect(self.update_finished)
+        import_signals.loading.connect(self.update_loading)
+        import_signals.loading_failed.connect(self.report_failed_loading)
+        import_signals.import_finished.connect(self.update_finished)
 
         # Threads
         self.main_worker_thread = MainWorkerThread(self)
 
     def reset_progressbars(self) -> None:
-        self.finalizing_progressbar.reset()
+        self.loading_progressbar.reset()
         self.unarchive_progressbar.reset()
         self.finished_label.setText('')
 
@@ -246,8 +179,7 @@ class ImportPage(QtWidgets.QWizardPage):
     def initializePage(self):
         self.reset_progressbars()
         self.init_progressbars()
-        self.main_worker_thread.update(plugin_name=self.field('plugin_name'),
-                                       plugin_path=self.field('plugin_path'))
+        self.main_worker_thread.update(plugin_path=self.field('plugin_path'))
         self.main_worker_thread.start()
 
     @Slot()
@@ -256,45 +188,90 @@ class ImportPage(QtWidgets.QWizardPage):
         self.unarchive_progressbar.setValue(1)
 
     @Slot()
-    def update_finalizing(self) -> None:
-        self.finalizing_progressbar.setRange(0, 0)
+    def update_loading(self) -> None:
+        self.loading_progressbar.setRange(0, 0)
 
     @Slot()
-    def update_finished(self, plugin, name) -> None:
-        """Plugin import was successful, quit the thread and the wizard."""
+    def update_finished(self, plugin) -> None:
+        """Plugin import was successful, quit the thread."""
         if self.main_worker_thread.isRunning():
             self.main_worker_thread.quit()
-        self.finalizing_progressbar.setMaximum(1)
-        self.finalizing_progressbar.setValue(1)
-        self.finished_label.setText('<b>Finished!</b>')
+        self.loading_progressbar.setMaximum(1)
+        self.loading_progressbar.setValue(1)
+        self.finished_label.setText('<b>Ready ton import !</b>')
         self.complete = True
         self.completeChanged.emit()
-        signals.plugin_imported.emit(plugin, name)
+        self.wizard.plugin = plugin
 
     @Slot(str, name="handleUnzipFailed")
     def report_failed_unarchive(self, file: str) -> None:
         """Handle the issue where the 7z file is in some way corrupted.
-         """
+        """
         self.main_worker_thread.exit(1)
 
         error = (
             "Corrupted (.7z) archive",
             "The archive '{}' is corrupted, please remove and re-download it.".format(file),
         )
-        import_signals.import_failure_detailed.emit(
-            QtWidgets.QMessageBox.Warning, error
-        )
+        import_signals.import_failure.emit(error)
         return
+
+    @Slot(str, name="handleLoadingFailed")
+    def report_failed_loading(self) -> None:
+        """Handle the issue where the plugin contain errors.
+        """
+        self.main_worker_thread.exit(1)
+
+        error = (
+            "Broken plugin code",
+            "An error occured while loading plugin code",
+        )
+        import_signals.import_failure.emit(error)
+        return  
+
+    def nextId(self):
+        return PluginImportWizard.CONFIRM
+
+
+class ConfirmationPage(QtWidgets.QWizardPage):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setFinalPage(True)
+        self.wizard = parent
+        self.setCommitPage(True)
+        self.setButtonText(QtWidgets.QWizard.CommitButton, 'Import Plugin')
+        self.current_project_label = QtWidgets.QLabel('empty')
+        self.plugin_infos_label = QtWidgets.QLabel('empty')
+        layout = QtWidgets.QVBoxLayout()
+        box = QtWidgets.QGroupBox("Plugin Summary:")
+        box_layout = QtWidgets.QVBoxLayout()
+        box_layout.addWidget(self.current_project_label)
+        box_layout.addWidget(self.plugin_infos_label)
+        box.setLayout(box_layout)
+        box.setStyleSheet(style_group_box.border_title)
+        layout.addWidget(box)
+        self.setLayout(layout)
+
+    def initializePage(self):
+        self.current_project_label.setText(
+            'Current Project: <b>{}</b>'.format(bw.projects.current))
+        infos = ""
+        for key, value in self.wizard.plugin.infos.items():
+            infos = infos + ("<br><i>{}:</i> {}".format(key,value))
+        self.plugin_infos_label.setText(infos)
+
+    def validatePage(self):
+        signals.plugin_imported.emit(self.wizard.plugin, self.wizard.plugin.infos['id'])
+        return True
 
 
 class MainWorkerThread(QtCore.QThread):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.plugin_name = None
         self.plugin_path = None
+        self.plugin = None
 
-    def update(self, plugin_name: str, plugin_path=None, relink=None) -> None:
-        self.plugin_name = plugin_name
+    def update(self, plugin_path=None, relink=None) -> None:
         self.plugin_path = plugin_path
 
     def run(self) -> None:
@@ -338,18 +315,13 @@ class MainWorkerThread(QtCore.QThread):
 
 class ImportSignals(QtCore.QObject):
     extraction_progress = Signal(int, int)
-    strategy_progress = Signal(int, int)
-    finalizing = Signal()
-    finished = Signal(object, str)
     unarchive_finished = Signal()
     unarchive_failed = Signal(str)
-    download_complete = Signal()
+    loading = Signal()
+    loading_failed = Signal()
+    import_finished = Signal(object)
     import_failure = Signal(tuple)
-    import_failure_detailed = Signal(object, tuple)
     cancel_sentinel = False
-    login_success = Signal(bool)
-    connection_problem = Signal(tuple)
-    links_required = Signal(object, object)
 
 
 import_signals = ImportSignals()
